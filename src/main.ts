80baa46a035c8a2ec3e4d0b34d3f99168a60c639
Jeff <25332698+Jeff0Brewer@users.noreply.github.com>
2023-11-13 13:25:28 -0700

    add main fn, load textures async

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { loadImageAsync } from './lib/load'
 import Vis from './vis/vis'
 import './style.css'
 
-const vis = new Vis()
+const main = async (): Promise<void> => {
+    const imgPromises = [
+        loadImageAsync('./gt1-0.png'),
+        loadImageAsync('./gt1-1.png')
+    ]
+    const imgs = await Promise.all(imgPromises)
 
-const tick = (): void => {
-    vis.draw()
+    const vis = new Vis(imgs)
+
+    const tick = (): void => {
+        vis.draw()
+        window.requestAnimationFrame(tick)
+    }
     window.requestAnimationFrame(tick)
 }
-window.requestAnimationFrame(tick)
+
+main()
